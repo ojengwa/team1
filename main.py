a52d639a2a6cb7f116292f9a36acb1e23286c9d3
Segun <xtreem88@gmail.com>
2015-06-17 11:14:57 +0100

    Web scraping done

--- main.py
+++ main.py
@@ -1,13 +1,133 @@
-from BeautifulSoup import BeautifulSoup
 import urllib2
-def get_url(url):
-	page = urllib2.urlopen(url)
-	soup = BeautifulSoup(page)
+from BeautifulSoup import BeautifulSoup
+from urlparse import urljoin
+from urlparse import urlparse
+import re
 
-	return soup
+
+def analyse_web(root,max_depth):
+    #get root domain
+    
+    
+    #crawl
+    page1, stat= get_page(root)
+    external = get_external(root)
+    crawled = {}
+    for check in external:
+        if check != "":
+            domain = get_domain(check)
+        else:
+            continue
+
+        filter_domain = [domain]
+        #set domain and depth
+        tocrawl = [[check,1]]
+        
+        count=0;
+        
+        while tocrawl: 
+            crawl_ele = tocrawl.pop()
+            link = crawl_ele[0]
+            depth = crawl_ele[1]
+            
+            if link not in crawled.keys():
+                content, title = get_page(link)
+                
+                if content == None:
+                    #link = link.encode('latin-1')
+                    #title = title.encode('latin-1')
+                    crawled[link]= title
+                    continue
+                host = get_domain(link)
+                
+                
+                if depth < max_depth and host in filter_domain :
+
+                    outlinks = get_all_links(content,link)
+                    
+                    print '-----------------------------------'
+                    print 'Adding outlinks ' + str(outlinks) + ' for parent page '+link
+                    print '-----------------------------------'
+                    add_to_tocrawl(crawled.keys(),tocrawl, outlinks, depth+1)
+                
+                crawled[link]= title
+                
+        """f = open('site_health.txt', 'w')
+        for url,status in crawled.iteritems():
+            f.write(url)
+            f.write('\t')
+            f.write('\t')
+            f.write(status)
+            f.write('\n')
+        f.close()"""
+    print crawled
+
+def get_external(url):
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page)
+    return [l.get('href') for l in soup.findAll('a') if is_external(url,l.get('href'))]
+    
+
+def get_domain(url):
+    
+    hostname = urlparse(url).hostname
+    
+    if len(re.findall( r'[0-9]+(?:\.[0-9]+){3}', hostname)) > 0:
+        return hostname
+    elif len(hostname.split('.')) == 0:
+        hostname
+    elif hostname.find('www.') != -1:
+        return hostname.split('.')[0]
+    else:
+        return hostname.split('.')[1]
+
+def is_external(root,host):
+    if len(host) > 0:
+
+        if host[0] == '/' or host[0] == '#':
+            return False
+    host = urlparse(host).hostname
+    hostname = urlparse(root).hostname
+    if host == None:
+        return False
+    return host != hostname and host.find(hostname) == -1
+    
+def get_page(url):
+    #print url
+    try:
+        response = urllib2.urlopen(url)
+        soup = BeautifulSoup(response)
+        
+        return soup, soup.title.string
+    except urllib2.HTTPError,e:
+        return None, str(e.code)
+    except urllib2.URLError,e:
+        return None, 'Invalid Url'
+    except:
+        return None, 'Wrong Url'
+    
+def get_next_target(page,parent):
+    
+    page.findAll('a')
+    page.get('href')
+    start_link = page.find('<a href=')
+    if start_link == -1: 
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    url = urljoin(parent,url)
+    return url, end_quote
+
+def get_all_links(page,parent):
+    return [l.get('href') for l in page.findAll('a') ]
+    
 
 
-	#for link in soup.findAll('title'):
-	#	print link.get()
+def add_to_tocrawl(crawled, tocrawl, newlinks, depth):
+    for link in newlinks:
+        if link not in tocrawl and link not in crawled:
+            tocrawl.append([link,depth])
 
-get_url("http://andela.co")
+
+analyse_web('http://andela.co',2)
